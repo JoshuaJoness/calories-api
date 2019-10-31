bb658ea6935790d8ca698cc727d5b85ccf434acc
Joshua Jones <joshuajones@trentu.ca>
2019-10-31 17:31:53 -0400

    added env

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: false}))
